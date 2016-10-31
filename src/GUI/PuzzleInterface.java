6f226f6bae5b9fbfae22d359655b6d875a77291c
jotte <j.j.sonneveld@student.utwente.nl>
2016-10-31 15:04:47 +0100

    puzzle grid added in PuzzleInterface

--- src/GUI/PuzzleInterface.java
+++ src/GUI/PuzzleInterface.java
@@ -16,72 +16,71 @@ public class PuzzleInterface extends JFrame {
         JFrame puzzleFrame = new JFrame("Nonogram Puzzle Solver");
         setPuzzleFrameAttr(puzzleFrame, 1366, 768);
         addMainPanels(puzzleFrame);
+        puzzleFrame.setVisible(true);
     }
 
     public static void addMainPanels(JFrame puzzleFrame) {
         puzzleFrame.getContentPane().add(optionsPanel);
         puzzleFrame.getContentPane().add(puzzlePanel);
         optionsPanel.setBackground(Color.green);
-        puzzlePanel.setBackground(Color.red);
-        int a = 20;
-        int b = 20;
+        puzzlePanel.setBackground(Color.white);
+        int a = 10;
+        int b = 10;
         JPanel[][] panelHolder = new JPanel[3][3];
         puzzlePanel.setLayout(new GridLayout(3, 3));
-
-        for (int m = 0; m < 3; m++) {
-            for (int n = 0; n < 3; n++) {
-                panelHolder[m][n] = new JPanel();
-                puzzlePanel.add(panelHolder[m][n]);
-            }
-        }
-        panelHolder[0][1].setBackground(Color.BLUE);
-        panelHolder[1][0].setBackground(Color.BLUE);
-        panelHolder[1][1].add(new JPanel());
-
-
-        createPuzzle(panelHolder[1][1], a, b);
+        createPuzzle(puzzlePanel, a, b);
     }
 
     public static void createPuzzle(JPanel puzzlePnl, int rows, int cols) {
-        puzzlePnl.setLayout(new GridBagLayout());
-        puzzlePnl.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
-        final int borderWidth = 2;
-        for (int row = 0; row < rows; row++) {
-            for (int col = 0; col < cols; col++) {
-                final JLabel label = new JLabel("");
-                if (row == 0) {
-                    if (col == 0) {
-                        // Top left corner, draw all sides
-                        label.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
-                    } else {
-                        // Top edge, draw all sides except left edge
-                        label.setBorder(BorderFactory.createMatteBorder(borderWidth,
-                                0,
-                                borderWidth,
-                                borderWidth,
-                                Color.BLACK));
-                    }
-                } else {
-                    if (col == 0) {
-                        // Left-hand edge, draw all sides except top
-                        label.setBorder(BorderFactory.createMatteBorder(0,
-                                borderWidth,
-                                borderWidth,
-                                borderWidth,
-                                Color.BLACK));
-                    } else {
-                        // Neither top edge nor left edge, skip both top and left lines
-                        label.setBorder(BorderFactory.createMatteBorder(0,
-                                0,
-                                borderWidth,
-                                borderWidth,
-                                Color.BLACK));
-                    }
-                }
-                puzzlePnl.add(label);
-            }
+        puzzlePnl.setLayout(new GridLayout(rows, cols));
+        for (int i = 0; i < (rows * cols); i++) {
+            final JLabel label = new JLabel("");
+            label.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+            puzzlePnl.add(label);
         }
+
     }
+
+//    public static void createPzzl(JPanel puzzlePnl, int rows, int cols) {
+//        puzzlePnl.setLayout(new GridLayout(rows, cols));
+//        puzzlePnl.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+//        final int borderWidth = 2;
+//        for (int row = 0; row < rows; row++) {
+//            for (int col = 0; col < cols; col++) {
+//                final JLabel label = new JLabel("");
+//                if (row == 0) {
+//                    if (col == 0) {
+//                        // Top left corner, draw all sides
+//                        label.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.BLACK));
+//                    } else {
+//                        // Top edge, draw all sides except left edge
+//                        label.setBorder(BorderFactory.createMatteBorder(borderWidth,
+//                                0,
+//                                borderWidth,
+//                                borderWidth,
+//                                Color.BLACK));
+//                    }
+//                } else {
+//                    if (col == 0) {
+//                        // Left-hand edge, draw all sides except top
+//                        label.setBorder(BorderFactory.createMatteBorder(0,
+//                                borderWidth,
+//                                borderWidth,
+//                                borderWidth,
+//                                Color.BLACK));
+//                    } else {
+//                        // Neither top edge nor left edge, skip both top and left lines
+//                        label.setBorder(BorderFactory.createMatteBorder(0,
+//                                0,
+//                                borderWidth,
+//                                borderWidth,
+//                                Color.BLACK));
+//                    }
+//                }
+//                puzzlePnl.add(label);
+//            }
+//        }
+//    }
 
     /**
      * Sets some attributes for the main frame
