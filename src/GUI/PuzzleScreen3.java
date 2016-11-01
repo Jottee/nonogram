563fe319826da5025fa094aafba7a1c452be7d80
jotte <j.j.sonneveld@student.utwente.nl>
2016-11-01 14:05:40 +0100

    create puzzle met input bijna werkend

---
+++ src/GUI/PuzzleScreen3.java
@@ -0,0 +1,171 @@
+package GUI;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class PuzzleScreen3 extends JFrame {
+
+    /**
+     * Constructor of the frame, calls createGUI which initializes the elements of the GUI
+     */
+    public PuzzleScreen3() {
+        super("Nonogram Puzzle Solver");
+        createGUI();
+    }
+
+    /**
+     * Initialize the elements of the GUI
+     */
+    public void createGUI() {
+        //Create the two main panels
+        JPanel puzzlePanel = new JPanel();
+        JPanel optionsPanel = new JPanel();
+
+        //add panels and set options for the panels
+        getContentPane().setLayout(new GridLayout(1, 2));
+        getContentPane().add(optionsPanel);
+        getContentPane().add(puzzlePanel);
+        optionsPanel.setBackground(Color.gray);
+        puzzlePanel.setBackground(Color.lightGray);
+        optionsPanel.setLayout(new FlowLayout(FlowLayout.CENTER));
+
+
+        //Create a simple text input (for testing at the moment) and add actionlistener
+        JTextField rowInput = new JTextField(10);
+        //rowInput.addActionListener(new DisplayTextAL());
+        optionsPanel.add(rowInput);
+        JTextField colInput = new JTextField(10);
+        optionsPanel.add(colInput);
+        JButton okButton = new JButton("OK");
+        okButton.addActionListener(new ActionListener() {
+
+            @Override
+            public void actionPerformed(ActionEvent actionEvent) {
+                try {
+                    int rows = Integer.parseInt(rowInput.getText());
+                    int cols = Integer.parseInt(colInput.getText());
+                    createPuzzleField(testGBag, rows, cols);
+                    System.out.println("Clicked OK");
+                } catch (NumberFormatException e) {
+                    JOptionPane.showMessageDialog(null, "Not a number");
+                }
+            }
+        });
+
+
+        optionsPanel.add(okButton, BorderLayout.CENTER);
+
+
+        // createPuzzleField(testGBag, rows, cols);
+    }
+
+    /**
+     * Creates a field of the nonogram with specified size
+     *
+     * @param panel panel that the puzzle will be on
+     * @param rows  amount of rows
+     * @param cols  amount of cols
+     */
+    public void createPuzzleField(JPanel panel, int rows, int cols) {
+
+        //add new panel with GridBagLayout
+        JPanel testGBag = new JPanel(new GridBagLayout());
+        testGBag.setBackground(Color.lightGray);
+        puzzlePanel.add(testGBag);
+
+        //set dimensions and settings of mid panel
+        GridBagConstraints cMid = new GridBagConstraints();
+        JPanel midPanel = new JPanel();
+        midPanel.setBackground(Color.white);
+        cMid.ipady = rows * 17;
+        cMid.ipadx = cols * 17;
+        cMid.weightx = 0.0;
+        cMid.weighty = 0.0;
+        cMid.gridwidth = 1;
+        cMid.gridheight = 1;
+        cMid.gridx = 1;
+        cMid.gridy = 1;
+        cMid.fill = cMid.BOTH;
+        midPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.black));
+        panel.add(midPanel, cMid);
+
+        //set dimensions and settings of top panel
+        GridBagConstraints cTop = new GridBagConstraints();
+        JPanel topPanel = new JPanel();
+        topPanel.setBackground(Color.orange);
+        cTop.ipady = 0;
+        cTop.ipadx = 0;
+        cTop.weightx = 0.0;
+        cTop.weighty = 0.0;
+        cTop.gridwidth = 1;
+        cTop.gridheight = 1;
+        cTop.gridx = 1;
+        cTop.gridy = 0;
+        topPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 0, 1, Color.black));
+        panel.add(topPanel, cTop);
+
+
+        GridBagConstraints cLeft = new GridBagConstraints();
+        JPanel leftPanel = new JPanel();
+        leftPanel.setBackground(Color.yellow);
+        cLeft.ipady = 0;
+        cLeft.ipadx = 0;
+        cLeft.weightx = 0.0;
+        cLeft.weighty = 0.0;
+        cLeft.gridwidth = 1;
+        cLeft.gridheight = 1;
+        cLeft.gridx = 0;
+        cLeft.gridy = 1;
+        leftPanel.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 0, Color.black));
+        panel.add(leftPanel, cLeft);
+
+        leftPanel.setLayout(new GridBagLayout());
+        topPanel.setLayout(new GridBagLayout());
+        JTextField jtf;
+        cLeft.ipady = 2;
+        cLeft.ipadx = 170;
+        for (int i = 0; i < rows; i++) {
+            jtf = new JTextField();
+            cLeft.gridy = i;
+            jtf.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 0, Color.gray));
+            leftPanel.add(jtf, cLeft);
+        }
+        JTextArea jta;
+        cTop.ipady = 100;
+        cTop.ipadx = 17;
+        for (int i = 0; i < cols; i++) {
+            jta = new JTextArea();
+            cTop.gridx = i;
+            jta.setBorder(BorderFactory.createMatteBorder(1, 1, 0, 1, Color.gray));
+            topPanel.add(jta, cTop);
+        }
+
+        midPanel.setLayout(new GridLayout(rows, cols));
+        for (int i = 0; i < (rows * cols); i++) {
+            final JLabel label = new JLabel("");
+            label.setBorder(BorderFactory.createMatteBorder(1, 1, 1, 1, Color.gray));
+            midPanel.add(label);
+        }
+        setVisible(true);
+    }
+
+    public static void main(String[] args) {
+        //Initialize the screen
+        JFrame fr = new PuzzleScreen3();
+        //Change some variables for the frame
+        fr.setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+        fr.pack();
+        fr.setVisible(true);
+        fr.setSize(1366, 768);
+    }
+
+    public class CreatePuzzleAL implements ActionListener {
+
+        @Override
+        public void actionPerformed(ActionEvent actionEvent) {
+        }
+    }
+}
+
